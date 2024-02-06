50d9eb6696e45899b49d45e5f26b6d5c527a742f
Brad Jasper <bjasper@gmail.com>
2024-02-05 16:26:07 -0800

    analytics

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 #
+NAME="llm.js"
+DOMAIN="llmjs.themaximalist.com"
 TITLE="LLM.js — Simple LLM JavaScript Library"
 DESCRIPTION="LLM.js is a simple JavaScript library for interacting with LLMs like OpenAI, Claude and LLamafile"
 
-readme-to-index "$TITLE" "$DESCRIPTION"
+readme-to-index "$NAME" "$DOMAIN" "$TITLE" "$DESCRIPTION"
