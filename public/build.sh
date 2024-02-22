914b79ffea183f95b645ceb8e34c982b3136c3d6
Brad Jasper <bjasper@gmail.com>
2024-02-22 15:24:40 -0800

    added gap headers custom classes

--- public/build.sh
+++ public/build.sh
@@ -4,5 +4,6 @@ NAME="llm.js"
 DOMAIN="llmjs.themaximalist.com"
 TITLE="LLM.js — Simple LLM library for Node.js "
 DESCRIPTION="Use dozens of Large Language Models your apps (like GPT-4, Gemini, Claude and more)"
+CLASSES="gap-2xl"
 
-readme-to-index "$NAME" "$DOMAIN" "$TITLE" "$DESCRIPTION"
+readme-to-index "$NAME" "$DOMAIN" "$TITLE" "$DESCRIPTION" "$CLASSES"
