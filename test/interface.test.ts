191debcb677590b54681934412fcccf7c1e196bf
bradintelemodel <brad@intelemodel.com>
2025-05-31 15:09:19 -0500

    refactoring started

---
+++ test/interface.test.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from "vitest";
+// import LLM from "../src/index.js";
+// import { delay } from "../src/utils.js";
+
+// initialize object
+// short-hand
+
+// fetch latest model info
+
+describe("LLM Interface", function () {
+    it("simple prompt", async function () {
+        expect(2).toBe(2);
+    });
+});
