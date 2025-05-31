8c21d6c8285a6eae0bbed37a7b411748738089bd
bradintelemodel <brad@intelemodel.com>
2025-05-31 18:47:07 -0500

    anthropic chat

--- test/chat.test.ts
+++ test/chat.test.ts
@@ -1,17 +1,14 @@
 import { describe, it, expect } from "vitest";
-import LLM, { Anthropic, Ollama, type Message } from "../src/index.js";
-
-// fetch latest model info
+import LLM from "../src/index.js";
 
 describe("chat", function () {
-    it.only("ollama", async function () {
-        const response = await LLM("in one word the color of the sky is usually", { max_tokens: 1 });
-        expect(response).toBeDefined();
-        expect(response.length).toBeGreaterThan(0);
-        expect(response.toLowerCase()).toEqual("blue");
+    const services = ["ollama", "anthropic"];
+    services.forEach(service => {
+        it.only(service, async function () {
+            const response = await LLM("in one word the color of the sky is usually", { max_tokens: 1 });
+            expect(response).toBeDefined();
+            expect(response.length).toBeGreaterThan(0);
+            expect(response.toLowerCase()).toEqual("blue");
+        });
     });
-
-
-    // max tokens
-    // stream extended
 });
